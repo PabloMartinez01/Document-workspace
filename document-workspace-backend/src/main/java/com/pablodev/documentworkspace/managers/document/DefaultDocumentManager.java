4fb003b010f8e592167ce4d44b7632ee1fb2a0b2
Pablodev <pablomartinezcoder@gmail.com>
2025-02-24 19:28:53 +0100

    Feat: All formats compatibilty

--- document-workspace-backend/src/main/java/com/pablodev/documentworkspace/managers/document/DefaultDocumentManager.java
+++ document-workspace-backend/src/main/java/com/pablodev/documentworkspace/managers/document/DefaultDocumentManager.java
@@ -1,14 +1,46 @@
 package com.pablodev.documentworkspace.managers.document;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onlyoffice.model.common.Format;
+import com.onlyoffice.model.documenteditor.config.document.DocumentType;
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Component
 public class DefaultDocumentManager implements DocumentManager  {
+
+    private List<Format> formats;
+
+    @PostConstruct
+    public void postConstruct() throws IOException {
+        ClassPathResource resource = new ClassPathResource("assets/document-formats.json");
+        this.formats = new ObjectMapper()
+                .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+                .readValue(resource.getInputStream(),new TypeReference<>() {});
+    }
+
+    @Override
+    public String getDocumentExtension(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(f.lastIndexOf(".") + 1))
+                .map(String::toLowerCase)
+                .filter(Predicate.not(String::isEmpty))
+                .map(String::toLowerCase)
+                .orElse(null);
+    }
 
     @Override
     public String getDocumentKey(Long documentId) {
@@ -24,4 +56,34 @@ public class DefaultDocumentManager implements DocumentManager  {
         }
     }
 
+    @Override
+    public DocumentType getDocumentType(String filename) {
+        return Optional.ofNullable(getDocumentExtension(filename))
+                .flatMap(ext -> formats.stream()
+                        .filter(format -> format.getName().equals(ext))
+                        .map(Format::getType)
+                        .findFirst())
+                .orElse(null);
+    }
+
+    @Override
+    public boolean isEditable(String filename) {
+       return Optional.ofNullable(getDocumentExtension(filename))
+               .flatMap(ext -> formats.stream()
+                       .filter(format -> format.getName().equals(ext))
+                       .map(format -> format.getActions().contains("edit"))
+                       .findFirst())
+               .orElse(false);
+    }
+
+
+    @Override
+    public boolean isViewable(String filename) {
+        return Optional.ofNullable(getDocumentExtension(filename))
+                .flatMap(ext -> formats.stream()
+                        .filter(format -> format.getName().equals(ext))
+                        .map(format -> format.getActions().contains("view"))
+                        .findFirst())
+                .orElse(false);
+    }
 }
