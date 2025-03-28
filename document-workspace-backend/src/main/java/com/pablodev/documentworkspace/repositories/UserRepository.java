b805bec0005969160bbfa161f5bb56a63d98e6ad
Pablodev <pablomartinezcoder@gmail.com>
2025-03-28 13:51:52 +0100

    Feat: Add user service and repository

---
+++ document-workspace-backend/src/main/java/com/pablodev/documentworkspace/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pablodev.documentworkspace.repositories;
+
+import com.pablodev.documentworkspace.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
