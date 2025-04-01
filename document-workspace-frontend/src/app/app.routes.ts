2a845d6debf6d572d089f75fe30c354f3fa67de9
Pablodev <pablomartinezcoder@gmail.com>
2025-04-01 17:05:58 +0200

    Feat: UI login page

--- document-workspace-frontend/src/app/app.routes.ts
+++ document-workspace-frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import {Routes} from '@angular/router';
+import {LoginComponent} from './features/auth/pages/login/login.component';
 
 export const routes: Routes = [
   { path: 'documents', loadChildren: () => import('./features/documents/documents.module').then(m => m.DocumentsModule) },
   { path: 'folder', loadChildren: () => import('./features/folder/folder.module').then(m => m.FolderModule) },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'folder/1' }
 ];
