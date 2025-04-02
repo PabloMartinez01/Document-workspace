ea91445adcc2124be5d35ddea79d444177cdb41a
Pablodev <pablomartinezcoder@gmail.com>
2025-04-02 18:38:26 +0200

    Feat: Authentication service

---
+++ document-workspace-frontend/src/app/core/services/authentication.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AuthenticationRequest} from '../model/authentication/authentication-request';
+import {Observable} from 'rxjs';
+import {AuthenticationResponse} from '../model/authentication/authentication-response';
+import {environment} from '../../../environments/environment';
+import {TokenStorageService} from './token-storage.service';
+import {jwtDecode} from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private httpClient: HttpClient, private tokenStorageService: TokenStorageService) {
+
+  }
+
+  authenticate(authenticationRequest: AuthenticationRequest): Observable<AuthenticationResponse> {
+    return this.httpClient.post<AuthenticationResponse>(environment.documentService + "/authenticate", authenticationRequest)
+  }
+
+  isTokenValid(): boolean {
+    const token = this.tokenStorageService.getToken();
+    if (!token) return false;
+    try {
+      const decodedToken: any = jwtDecode(token);
+      if (!decodedToken) return false;
+      const expirationDate = decodedToken.payload.exp * 1000;
+      return expirationDate > Date.now();
+    } catch (error) {
+      return false;
+    }
+  }
+
+  isAuthenticated(): boolean {
+    return this.isTokenValid();
+  }
+
+
+
+
+}
