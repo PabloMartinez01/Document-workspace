269b7b9223ee1eae7891861ea03de46e5d3b55d0
Pablodev <pablomartinezcoder@gmail.com>
2025-04-06 19:05:11 +0200

    Style: Modify general styles

---
+++ document-workspace-frontend/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+import {AuthenticationService} from '../../core/services/authentication.service';
+import {Router} from '@angular/router';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'sidebar',
+  standalone: true,
+  imports: [
+    NgIf
+  ],
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+
+  ) {
+
+  }
+
+  logout(): void {
+    this.authenticationService.logout();
+    this.router.navigate(['login']).then();
+  }
+
+  isAuthenticated(): boolean {
+    return this.authenticationService.isAuthenticated();
+  }
+}
