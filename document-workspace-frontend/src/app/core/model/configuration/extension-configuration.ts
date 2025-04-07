cea18d55dfb67a4f16606b018bdcf4508f325cd1
Pablodev <pablomartinezcoder@gmail.com>
2025-04-07 18:44:47 +0200

    Feat: Add file types in extensions.json

--- document-workspace-frontend/src/app/core/model/configuration/extension-configuration.ts
+++ document-workspace-frontend/src/app/core/model/configuration/extension-configuration.ts
@@ -2,4 +2,8 @@
 export interface ExtensionConfiguration {
   editableFormats: string[];
   viewableFormats: string[];
+  documentFormats: string[];
+  slideFormats: string[];
+  spreadsheetFormats: string[];
+  formFormats: string[];
 }
