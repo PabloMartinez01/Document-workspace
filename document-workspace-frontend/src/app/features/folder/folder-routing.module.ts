de1ba771c745e2ba45a2d7fdca6177d023f0856e
Pablodev <pablomartinezcoder@gmail.com>
2025-04-09 17:35:38 +0200

    Fix: Auth guard for params change

--- document-workspace-frontend/src/app/features/folder/folder-routing.module.ts
+++ document-workspace-frontend/src/app/features/folder/folder-routing.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ViewFolderComponent} from './pages/view-folder/view-folder.component';
+import {authGuard} from '../../core/guard/auth.guard';
 
 
 const routes: Routes = [
-  { path: ':id', component: ViewFolderComponent },
+  {
+    path: ':id',
+    component: ViewFolderComponent,
+    canActivate: [authGuard],
+    runGuardsAndResolvers: 'paramsChange'
+  },
 ];
 
 @NgModule({
