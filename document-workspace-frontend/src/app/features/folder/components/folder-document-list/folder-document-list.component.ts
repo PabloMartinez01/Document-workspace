9303478971f40cd1872ca1dc3f5b12cefba03f43
Pablodev <pablomartinezcoder@gmail.com>
2025-04-17 22:45:57 +0200

    Feat: Upgrade to java 23

--- document-workspace-frontend/src/app/features/folder/components/folder-document-list/folder-document-list.component.ts
+++ document-workspace-frontend/src/app/features/folder/components/folder-document-list/folder-document-list.component.ts
@@ -96,15 +96,9 @@ export class FolderDocumentListComponent {
   }
 
   bytesToString(bytes: number): string {
-    if (bytes < 1024) {
-      return `${bytes} bytes`;
-    }
-    if (bytes < 1024 * 1024) {
-      return `${(bytes / 1024).toFixed(2)} KB`;
-    }
-    if (bytes < 1024 * 1024 * 1024) {
-      return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
-    }
+    if (bytes < 1024) return `${bytes} bytes`;
+    if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`;
+    if (bytes < 1024 * 1024 * 1024) return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
     return `${(bytes / (1024 * 1024 * 1024)).toFixed(2)} GB`;
   }
 
