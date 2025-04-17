fb9adc01f47db6ef10871cff234ce3827f6892e8
Pablodev <pablomartinezcoder@gmail.com>
2025-04-17 12:19:58 +0200

    Refactor: Remove unnecessary components

--- document-workspace-frontend/src/app/app.component.ts
+++ document-workspace-frontend/src/app/app.component.ts
@@ -6,7 +6,6 @@ import {
 } from "./features/folder/components/folder-document-list/folder-document-list.component";
 import {FolderInfoComponent} from "./features/folder/components/folder-info/folder-info.component";
 import {FolderListComponent} from "./features/folder/components/folder-list/folder-list.component";
-import {FolderToolbarComponent} from "./features/folder/components/folder-toolbar/folder-toolbar.component";
 import {NgIf} from "@angular/common";
 import {SidebarComponent} from "./shared/sidebar/sidebar.component";
 import {UploadDropzoneComponent} from "./features/folder/components/upload-dropzone/upload-dropzone.component";
@@ -15,7 +14,7 @@ import {UploadDropzoneComponent} from "./features/folder/components/upload-dropz
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, DocumentEditorModule, FolderDocumentListComponent, FolderInfoComponent, FolderListComponent, FolderToolbarComponent, NgIf, SidebarComponent, UploadDropzoneComponent],
+    imports: [RouterOutlet, DocumentEditorModule, FolderDocumentListComponent, FolderInfoComponent, FolderListComponent, NgIf, SidebarComponent, UploadDropzoneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
