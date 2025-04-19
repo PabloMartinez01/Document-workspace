4518636f855c211d0e77932bfe21ef7d436c3f23
Pablodev <pablomartinezcoder@gmail.com>
2025-04-19 17:45:57 +0200

    Feat: Change Document relationships

--- document-workspace-backend/src/main/java/com/pablodev/documentworkspace/services/extension/ExtensionService.java
+++
@@ -1,7 +0,0 @@
-package com.pablodev.documentworkspace.services.extension;
-
-import com.pablodev.documentworkspace.dto.extension.ExtensionResponse;
-
-public interface ExtensionService {
-    ExtensionResponse findExtensionByName(String name);
-}
